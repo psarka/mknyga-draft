5796ed91e28aeda5c1604abaabab2e0d8544df50
sqrt <devnull@localhost>
2011-09-07 12:58:18 +0300

    patvarkytos smulkmenos

--- parse.py
+++ parse.py
@@ -34,61 +34,59 @@ def parse (inputf, outputf):
                 out = open("sprendimai.tex", 'a') 
                 outC= open("sprendimaiC.tex",'a')
                 eile= ""
-            
-            i = eile.find("chapter",0)
-            if (i!= -1):
-        
-                j = eile.find("}",i);
-                out.write("\section*" + eile[i+7:j-i+2] + "\n")
-                outC.write("\section*" + eile[i+7:j-i+2] + "\n")
-            
-            i = eile.find("\section",0)
-            if (i!= -1): 
-                        
-                j = eile.find("}",i);
-                out.write("\subsection*" + eile[i+8:j-i+2] + "\n")
-                outC.write("\subsection*" + eile[i+8:j-i+2] + "\n")
-
-            i = eile.find("\subsection",0)
-            if (i!= -1): 
-        
-                j = eile.find("}",i);
-                out.write("\subsubsection*" + eile[i+11:j-i+1]+"\n")
-                outC.write("\subsubsection*" + eile[i+11:j-i+1]+"\n")
-
-            i = eile.find("\end{enumerate}",0)
-            if (i!= -1): 
-                if onoff: 
-                    out.write("\end{enumerate} \n")
-                    outC.write("\end{enumerate} \n")
-                    onoff = 0
-
-            i = eile.find(u"\subsubsection{Uždaviniai}".encode('utf8'),0);
-            if (i!= -1): 
-                #pdb.set_trace()
-                onoff = 1
-                out.write("\\begin{enumerate} \n");
-                outC.write("\\begin{enumerate} \n");
-
-            label+=1
-            i = eile.find("\item",0);
-            if i!= -1 and onoff: 
-
-                link = inputf + str(label) #nuorodos relizuojamos per \label{}
-                out.write("\item \n");
-                outC.write("\item" + "\label{" + link + "}" + "\mbox{} \marginpar{\hyperref[b" + link + "]{$\wedge$}} \n")
-                eile = eile[:i+5] + "\mbox{} \marginpar {\hyperref[" + link + "]{$S$}}" + "\label{b" + link + "}" + eile[i+5:]
-
 
             if re.match('\s*%', eile) and onoff:
                 i = eile.find("%",0)
                 out.write(eile[i+1:])   #viskas, kas užkomentuota uždavinių subsubsection
                 outC.write(eile[i+1:])  #keliauja į sprendimų failus be pirmo procento
                 eile = "";
+	    else:
 
-            if (eile != ""):
-                ot.write(eile)
-
+	        i = eile.find("chapter",0)
+	        if (i!= -1):
+            
+                    j = eile.find("}",i);
+                    out.write("\section*" + eile[i+7:j-i+2] + "\n")
+                    outC.write("\section*" + eile[i+7:j-i+2] + "\n")
+                
+                i = eile.find("\section",0)
+                if (i!= -1): 
+                            
+                    j = eile.find("}",i);
+                    out.write("\subsection*" + eile[i+8:j-i+2] + "\n")
+                    outC.write("\subsection*" + eile[i+8:j-i+2] + "\n")
+    
+                i = eile.find("\subsection",0)
+                if (i!= -1): 
+            
+                    j = eile.find("}",i);
+                    out.write("\subsubsection*" + eile[i+11:j-i+1]+"\n")
+                    outC.write("\subsubsection*" + eile[i+11:j-i+1]+"\n")
+    
+                i = eile.find("\end{enumerate}",0)
+                if (i!= -1): 
+                    if onoff: 
+                        out.write("\end{enumerate} \n")
+                        outC.write("\end{enumerate} \n")
+                        onoff = 0
+    
+                i = eile.find(u"\subsubsection{Uždaviniai}".encode('utf8'),0);
+                if (i!= -1): 
+                    onoff = 1
+                    out.write("\\begin{enumerate} \n");
+                    outC.write("\\begin{enumerate} \n");
+    
+                label+=1
+                i = eile.find("\item",0);
+                if i!= -1 and onoff: 
+    
+                    link = inputf + str(label) #nuorodos relizuojamos per \label{}
+                    out.write("\item \n");
+                    outC.write("\item" + "\label{" + link + "}" + "\mbox{} \marginpar{\hyperref[b" + link + "]{$\wedge$}} \n")
+                    eile = eile[:i+5] + "\mbox{} \marginpar {\hyperref[" + link + "]{$S$}}" + "\label{b" + link + "}" + eile[i+5:]
+                if (eile != ""):
+                    ot.write(eile)
+    
     ot.write("\n")
     iin.close()
     ot.close()
